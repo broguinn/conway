cafb89cae0b53b59d330a34ca78f1348dc92c721
Braden O'Guinn <nedarb906@gmail.com>
2013-10-29 15:07:57 -0700

    add a readme

---
+++ README.md
@@ -0,0 +1,5 @@
+#Conway's Game of Life
+
+This version includes stop/start, reset, step, templates, a demo, and high scores!
+
+Open up `index.html` to give it a go, or visit the site [here](http://braden.site44.com/gameoflife).
--- js/scripts.js
+++ js/scripts.js
@@ -26,7 +26,6 @@ var Game = {
 
 var Grid = {
 
-
   generations: 0,
   population: 0,
 
@@ -64,7 +63,7 @@ var Grid = {
           }
           if (y < 0) {
             var newY = this.maxY;
-          } 
+          }
           else if (y > this.maxY) {
             var newY = 0;
           } else {
@@ -148,7 +147,7 @@ var Cell = {
       } else {
         this.nextState = "dead";
       }
-    } 
+    }
   }
 };
 
@@ -163,10 +162,10 @@ $(function() {
     }
     $("tr").each(function(){
       for (var j = 0; j < x; j++) {
-        $(this).append("<td data='" + j + "'></td>"); 
-      }    
+        $(this).append("<td data='" + j + "'></td>");
+      }
     });
-    setColors(); 
+    setColors();
   };
 
   function epoch() {
@@ -179,22 +178,22 @@ $(function() {
       $("button#reset").addClass("static-reset");
     }
     game.grid.tick();
-    $("h4#generation").empty().append("Generations: " + 
+    $("h4#generation").empty().append("Generations: " +
       "<span class='scores'>" + game.grid.generations + "</span>");
-    $("h4#population").empty().append("Population: " + 
+    $("h4#population").empty().append("Population: " +
       "<span class='scores'>" + game.grid.population + "</span>");
     // $("button#reset").removeClass("static-reset");
     game.checkScores();
-    $("h4#generation-high").empty().append("Most generations: " + 
+    $("h4#generation-high").empty().append("Most generations: " +
       "<span class='scores'>" + game.mostGenerations + "</span>");
-    $("h4#population-high").empty().append("Highest population: " + 
+    $("h4#population-high").empty().append("Highest population: " +
        "<span class='scores'>" + game.highPopulation + "</span>");
     game.grid.setAllLiveNeighbors();
     game.grid.setAllNextStates();
   };
 
   function startTime() {
-    intervalId = window.setInterval(epoch, 300);  
+    intervalId = window.setInterval(epoch, 300);
   };
 
   function stopTime() {
@@ -222,7 +221,7 @@ $(function() {
 
   $("form#input").submit(function() {
     //2 lines below here
-    var size = $("input#grid-size").val();  
+    var size = $("input#grid-size").val();
     makeBoard(size, size);
     // makeBoard(50, 50)
     return false;
@@ -235,7 +234,7 @@ $(function() {
     game.grid.setAllLiveNeighbors();
     setColors();
     game.grid.setAllNextStates();
-    $("h4#population-high").empty().append("Highest population: " + 
+    $("h4#population-high").empty().append("Highest population: " +
        "<span class='scores'>" + game.highPopulation + "</span>");
   });
 
@@ -252,18 +251,18 @@ $(function() {
     clearInterval(intervalId);
   });
 
-  $("#reset").click(function() {     
+  $("#reset").click(function() {
     if (game.demo) {
       clearInterval(intervalId);
       game = Object.create(Game);
       makeBoard(50, 50);
-      $("h4#generation").empty().append("Generations: " + 
+      $("h4#generation").empty().append("Generations: " +
         "<span class='scores'>" + game.grid.generations + "</span>");
-      $("h4#population").empty().append("Population: " + 
+      $("h4#population").empty().append("Population: " +
         "<span class='scores'>" + game.grid.population + "</span>");
-      $("h4#generation-high").empty().append("Most generations: " + 
+      $("h4#generation-high").empty().append("Most generations: " +
         "<span class='scores'>" + game.mostGenerations + "</span>");
-      $("h4#population-high").empty().append("Highest population: " + 
+      $("h4#population-high").empty().append("Highest population: " +
          "<span class='scores'>" + game.highPopulation + "</span>");
     }
     clearInterval(intervalId);
@@ -272,10 +271,10 @@ $(function() {
     makeBoard(size, size);
     // makeBoard(50, 50);
     $("button#reset").removeClass("static-reset");
-    $("h4#static-problems").empty().append("Put on some blocks and press start!"); 
-    $("h4#generation").empty().append("Generations: " + 
+    $("h4#static-problems").empty().append("Put on some blocks and press start!");
+    $("h4#generation").empty().append("Generations: " +
       "<span class='scores'>" + game.grid.generations + "</span>");
-    $("h4#population").empty().append("Population: " + 
+    $("h4#population").empty().append("Population: " +
       "<span class='scores'>" + game.grid.population + "</span>");
   });
 
@@ -286,7 +285,7 @@ $(function() {
     }
   });
 
-  $("#demo").click(function() { 
+  $("#demo").click(function() {
     game.toggleDemo();
     //set up the game
     //2 lines below here
@@ -314,7 +313,6 @@ $(function() {
     })
   }
 
-
   function addPattern() {
     var xCoor = parseInt($(this).attr("data"));
     var yCoor = parseInt($(this).parent().attr("data"));
@@ -327,7 +325,7 @@ $(function() {
     game.grid.setAllLiveNeighbors();
     setColors();
     game.grid.setAllNextStates();
-    $("h4#population-high").empty().append("Highest population: " + 
+    $("h4#population-high").empty().append("Highest population: " +
      "<span class='scores'>" + game.highPopulation + "</span>");
     $("#grid").off("click", "td", addPattern);
     $("#click2").hide();
@@ -341,184 +339,182 @@ $(function() {
     $('td').css( 'cursor', 'pointer');
     $("#click1").hide();
     $("#click2").show();
-    $("#grid").on("click", "td", addPattern);     
+    $("#grid").on("click", "td", addPattern);
   });
 });
 
-var demo = [[8, 12],  
-          [8, 13],  
-          [8, 14],  
-          [8, 15],  
-          [8, 16],  
-          [9, 11],  
-          [9, 17],  
-          [9, 34],  
-          [9, 41],  
-          [10, 10],  
-          [10, 18],  
-          [10, 34],  
-          [10, 35],  
-          [10, 36],  
-          [10, 37],  
-          [10, 38],  
-          [10, 39],  
-          [10, 40],  
-          [10, 41],  
-          [11, 10],  
-          [11, 18],  
-          [11, 34],  
-          [11, 41],  
-          [12, 10],  
-          [12, 15],  
-          [12, 18],  
-          [12, 41],  
-          [13, 11],  
-          [13, 15],  
-          [13, 16],  
-          [13, 17],  
-          [13, 41],  
-          [14, 15],  
-          [14, 41],  
-          [15, 41],  
-          [16, 18],  
-          [17, 16],  
-          [17, 17],  
-          [17, 18],  
-          [18, 13],  
-          [18, 14],  
-          [18, 15],  
-          [18, 17],  
-          [19, 12],  
-          [19, 17],  
-          [19, 24],  
-          [19, 25],  
-          [19, 26],  
-          [19, 27],  
-          [19, 28],  
-          [19, 35],  
-          [19, 41],  
-          [20, 13],  
-          [20, 14],  
-          [20, 15],  
-          [20, 17],  
-          [20, 23],  
-          [20, 29],  
-          [20, 35],  
-          [20, 41],  
-          [21, 16],  
-          [21, 17],  
-          [21, 18],  
-          [21, 23],  
-          [21, 29],  
-          [21, 35],  
-          [21, 36],  
-          [21, 37],  
-          [21, 38],  
-          [21, 39],  
-          [21, 40],  
-          [21, 41],  
-          [22, 18],  
-          [22, 23],  
-          [22, 29],  
-          [22, 35],  
-          [22, 41],  
-          [23, 23],  
-          [23, 29],  
-          [23, 35],  
-          [23, 41],  
-          [24, 24],  
-          [24, 25],  
-          [24, 26],  
-          [24, 27],  
-          [24, 28],  
-          [25, 18],  
-          [26, 14],  
-          [26, 15],  
-          [26, 16],  
-          [26, 17],  
-          [26, 18],  
-          [27, 12],  
-          [27, 13],  
-          [27, 23],  
-          [27, 24],  
-          [27, 25],  
-          [27, 26],  
-          [27, 27],  
-          [27, 28],  
-          [27, 29],  
-          [27, 35],  
-          [27, 36],  
-          [27, 37],  
-          [27, 38],  
-          [27, 39],  
-          [27, 40],  
-          [27, 41],  
-          [28, 13],  
-          [28, 14],  
-          [28, 23],  
-          [28, 26],  
-          [28, 35],  
-          [28, 38],  
-          [29, 15],  
-          [29, 23],  
-          [29, 26],  
-          [29, 35],  
-          [29, 38],  
-          [30, 13],  
-          [30, 14],  
-          [30, 23],  
-          [30, 26],  
-          [30, 35],  
-          [30, 38],  
-          [31, 12],  
-          [31, 13],  
-          [31, 23],  
-          [31, 35],  
-          [32, 14],  
-          [32, 15],  
-          [32, 16],  
-          [32, 17],  
-          [32, 18],  
-          [33, 18],  
-          [35, 35],  
-          [35, 41],  
-          [36, 12],  
-          [36, 18],  
-          [36, 35],  
-          [36, 36],  
-          [36, 37],  
-          [36, 38],  
-          [36, 39],  
-          [36, 40],  
-          [36, 41],  
-          [37, 12],  
-          [37, 13],  
-          [37, 14],  
-          [37, 15],  
-          [37, 16],  
-          [37, 17],  
-          [37, 18],  
-          [37, 35],  
-          [37, 38],  
-          [37, 41],  
-          [38, 12],  
-          [38, 15],  
-          [38, 18],  
-          [38, 35],  
-          [38, 38],  
-          [38, 41],  
-          [39, 12],  
-          [39, 15],  
-          [39, 18],  
-          [39, 35],  
-          [39, 38],  
-          [39, 41],  
-          [40, 12],  
-          [40, 15],  
-          [40, 18],  
-          [40, 35],  
-          [40, 41],  
-          [41, 12],  
-          [41, 18]] 
- 
-
+var demo = [[8, 12],
+          [8, 13],
+          [8, 14],
+          [8, 15],
+          [8, 16],
+          [9, 11],
+          [9, 17],
+          [9, 34],
+          [9, 41],
+          [10, 10],
+          [10, 18],
+          [10, 34],
+          [10, 35],
+          [10, 36],
+          [10, 37],
+          [10, 38],
+          [10, 39],
+          [10, 40],
+          [10, 41],
+          [11, 10],
+          [11, 18],
+          [11, 34],
+          [11, 41],
+          [12, 10],
+          [12, 15],
+          [12, 18],
+          [12, 41],
+          [13, 11],
+          [13, 15],
+          [13, 16],
+          [13, 17],
+          [13, 41],
+          [14, 15],
+          [14, 41],
+          [15, 41],
+          [16, 18],
+          [17, 16],
+          [17, 17],
+          [17, 18],
+          [18, 13],
+          [18, 14],
+          [18, 15],
+          [18, 17],
+          [19, 12],
+          [19, 17],
+          [19, 24],
+          [19, 25],
+          [19, 26],
+          [19, 27],
+          [19, 28],
+          [19, 35],
+          [19, 41],
+          [20, 13],
+          [20, 14],
+          [20, 15],
+          [20, 17],
+          [20, 23],
+          [20, 29],
+          [20, 35],
+          [20, 41],
+          [21, 16],
+          [21, 17],
+          [21, 18],
+          [21, 23],
+          [21, 29],
+          [21, 35],
+          [21, 36],
+          [21, 37],
+          [21, 38],
+          [21, 39],
+          [21, 40],
+          [21, 41],
+          [22, 18],
+          [22, 23],
+          [22, 29],
+          [22, 35],
+          [22, 41],
+          [23, 23],
+          [23, 29],
+          [23, 35],
+          [23, 41],
+          [24, 24],
+          [24, 25],
+          [24, 26],
+          [24, 27],
+          [24, 28],
+          [25, 18],
+          [26, 14],
+          [26, 15],
+          [26, 16],
+          [26, 17],
+          [26, 18],
+          [27, 12],
+          [27, 13],
+          [27, 23],
+          [27, 24],
+          [27, 25],
+          [27, 26],
+          [27, 27],
+          [27, 28],
+          [27, 29],
+          [27, 35],
+          [27, 36],
+          [27, 37],
+          [27, 38],
+          [27, 39],
+          [27, 40],
+          [27, 41],
+          [28, 13],
+          [28, 14],
+          [28, 23],
+          [28, 26],
+          [28, 35],
+          [28, 38],
+          [29, 15],
+          [29, 23],
+          [29, 26],
+          [29, 35],
+          [29, 38],
+          [30, 13],
+          [30, 14],
+          [30, 23],
+          [30, 26],
+          [30, 35],
+          [30, 38],
+          [31, 12],
+          [31, 13],
+          [31, 23],
+          [31, 35],
+          [32, 14],
+          [32, 15],
+          [32, 16],
+          [32, 17],
+          [32, 18],
+          [33, 18],
+          [35, 35],
+          [35, 41],
+          [36, 12],
+          [36, 18],
+          [36, 35],
+          [36, 36],
+          [36, 37],
+          [36, 38],
+          [36, 39],
+          [36, 40],
+          [36, 41],
+          [37, 12],
+          [37, 13],
+          [37, 14],
+          [37, 15],
+          [37, 16],
+          [37, 17],
+          [37, 18],
+          [37, 35],
+          [37, 38],
+          [37, 41],
+          [38, 12],
+          [38, 15],
+          [38, 18],
+          [38, 35],
+          [38, 38],
+          [38, 41],
+          [39, 12],
+          [39, 15],
+          [39, 18],
+          [39, 35],
+          [39, 38],
+          [39, 41],
+          [40, 12],
+          [40, 15],
+          [40, 18],
+          [40, 35],
+          [40, 41],
+          [41, 12],
+          [41, 18]]
